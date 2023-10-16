53beff52cfaf542d5754fe080dd7d90cc75303f8
Eduardo Stefanello <eduardo@stefanello.cc>
2023-10-16 10:57:01 -0300

    width css

--- hide.js
+++ hide.js
@@ -1,43 +1,48 @@
-const history = document.getElementById("history")
+function main() {
+    const history = document.getElementById("history")
 
-const check = document.createElement("input")
-check.setAttribute("type", "checkbox")
-check.setAttribute("style", "margin-bottom: 10px; margin-left: -0.5px;")
-check.setAttribute("id", "hide_details")
-check.onchange = function (event) {
-    for (const child of history.children) {
-        if (child.getAttribute("class") != null) {
-            if (child.getAttribute("class").indexOf("has-notes") == -1 || child.innerHTML.indexOf("<p>registro de tempo</p>") >= 0) {
-                if (event.target.checked) {
-                    child.setAttribute("style", "display: none;")
+    if (!history) return
+
+    const check = document.createElement("input")
+    check.setAttribute("type", "checkbox")
+    check.setAttribute("style", "margin-bottom: 10px; margin-left: -0.5px;")
+    check.setAttribute("id", "hide_details")
+    check.onchange = function (event) {
+        for (const child of history.children) {
+            if (child.getAttribute("class") != null) {
+                if (child.getAttribute("class").indexOf("has-notes") == -1 || child.innerHTML.indexOf("<p>registro de tempo</p>") >= 0) {
+                    if (event.target.checked) {
+                        child.setAttribute("style", "display: none;")
+                    }
+                    else {
+                        child.removeAttribute("style")
+                    }
+                    chrome.storage.local.set({"checked": event.target.checked})
                 }
-                else {
-                    child.removeAttribute("style")
-                }
-                chrome.storage.local.set({"checked": event.target.checked})
             }
         }
     }
-}
-chrome.storage.local.get(["checked"], function(items){
-    if (items.checked) {
-        check.click()
+    chrome.storage.local.get(["checked"], function(items){
+        if (items.checked) {
+            check.click()
+        }
+    })
+
+    const label = document.createElement("label")
+    label.setAttribute("for", "hide_details")
+    label.setAttribute("style", "margin-top: 2px;")
+    label.innerText = "Ocultar detalhes sem comentários"
+
+    const div = document.createElement("div")
+    div.setAttribute("style", "display: flex;")
+    div.appendChild(check)
+    div.appendChild(label)
+
+    for (const child of history.children) {
+        if (child.nodeName != "H3") {
+            history.insertBefore(div, child)
+            break
+        }
     }
-})
-
-const label = document.createElement("label")
-label.setAttribute("for", "hide_details")
-label.setAttribute("style", "margin-top: 2px;")
-label.innerText = "Ocultar detalhes sem comentários"
-
-const div = document.createElement("div")
-div.setAttribute("style", "display: flex;")
-div.appendChild(check)
-div.appendChild(label)
-
-for (const child of history.children) {
-    if (child.nodeName != "H3") {
-        history.insertBefore(div, child)
-        break
-    }
 }
+main()
--- settings.js
+++ settings.js
@@ -2,26 +2,23 @@ const saveOptions = () => {
     chrome.storage.local.set({
         "redmineToken": document.getElementById("redmineToken").value,
         "checkObservadas": document.getElementById("checkObservadas").checked,
-        "checkNome": document.getElementById("checkNome").checked,
-        "checkWidth": document.getElementById("checkWidth").checked
+        "checkNome": document.getElementById("checkNome").checked
+    }, function () {
+        document.getElementById("alertSuccess").removeAttribute("style")
+        setTimeout(function () {
+            document.getElementById("alertSuccess").setAttribute("style", "display: none")
+        }, 2000)
     })
-
-    document.getElementById("alertSuccess").removeAttribute("style")
-    setTimeout(function () {
-        document.getElementById("alertSuccess").setAttribute("style", "display: none")    
-    }, 2000);
 }
 
 const restoreOptions = () => {
     chrome.storage.local.get([
         "redmineToken",
         "checkObservadas",
-        "checkNome",
-        "checkWidth"], function (items) {
+        "checkNome"], function (items) {
             document.getElementById("redmineToken").value = items.redmineToken
             document.getElementById("checkObservadas").checked = items.checkObservadas
             document.getElementById("checkNome").checked = items.checkNome
-            document.getElementById("checkWidth").checked = items.checkWidth
         })
 }
 
